f918a5158ff69d07aa600f426ea41bbe58d083d1
TYPO3 Integrator <vasyl@typo3.net.ua>
2018-08-09 20:16:43 +0300

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,1 +1,20 @@
-
+<?php
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Extend ext:news and tt_content',
+    'description' => 'Extend ext:news the new content elements - answer by a relation to a tt_content and tt_content by a relation to a FE User',
+    'category' => 'fe',
+    'author' => 'Vasyl Mosiychuk',
+    'author_email' => 'vasyl@typo3.net.ua',
+    'author_company' => 'vasyl.net',
+    'state' => 'stable',
+    'clearCacheOnLoad' => 1,
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '8.7.0-9.9.99',
+            'news' => '6.0.0-7.9.99'
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
