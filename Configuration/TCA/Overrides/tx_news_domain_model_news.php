6c5941588ae09768fa1e200032411d5343e4d7e0
TYPO3 Integrator <vasyl@typo3.net.ua>
2018-08-09 19:48:44 +0300

    Update tx_news_domain_model_news.php

--- Configuration/TCA/Overrides/tx_news_domain_model_news.php
+++ Configuration/TCA/Overrides/tx_news_domain_model_news.php
@@ -1,2 +1,37 @@
 <?php
 defined('TYPO3_MODE') or die();
+$ll = 'LLL:EXT:news_answer/Resources/Private/Language/locallang_db.xlf:';
+$fields = [
+    'content_elements_answer' => [
+        'exclude' => true,
+        'label' => $ll . 'tx_news_domain_model_news.content_elements',
+        'config' => [
+            'type' => 'inline',
+            'allowed' => 'tt_content',
+            'foreign_table' => 'tt_content',
+            'foreign_sortby' => 'sorting',
+            'foreign_field' => 'tx_news_related_news',
+            'minitems' => 0,
+            'maxitems' => 99,
+            'appearance' => [
+                'useXclassedVersion' => $configuration->getContentElementPreview(),
+                'collapseAll' => true,
+                'expandSingle' => true,
+                'levelLinksPosition' => 'bottom',
+                'useSortable' => true,
+                'showPossibleLocalizationRecords' => true,
+                'showRemovedLocalizationRecords' => true,
+                'showAllLocalizationLink' => true,
+                'showSynchronizationLink' => true,
+                'enabledControls' => [
+                    'info' => false,
+                ]
+            ],
+            'behaviour' => [
+                'allowLanguageSynchronization' => true,
+            ],
+        ]
+    ]
+];
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tx_news_domain_model_news', $fields);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('tx_news_domain_model_news', 'content_elements_answer', '', 'after:content_elements');
