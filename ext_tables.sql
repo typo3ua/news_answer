ce03d58b3e70bd49c16374592f90a22090a5ddbb
TYPO3 Integrator <vasyl@typo3.net.ua>
2018-08-09 17:02:26 +0300

    Update ext_tables.sql

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,4 +1,12 @@
+#
+# Table structure for table 'tt_content'
+#
+CREATE TABLE tt_content (
+  tt_content_feuser_user varchar(100) DEFAULT '0' NOT NULL
+);
+#
 # Table structure for table 'tx_news_domain_model_news'
+#
 CREATE TABLE tx_news_domain_model_news (
-  tx_news_answer int(11) DEFAULT '0' NOT NULL
+  content_elements_answer int(11) DEFAULT '0' NOT NULL
 );
