65b157533561fdc7cf945d6ac0b957d0762bf7c0
TYPO3 Integrator <vasyl@typo3.net.ua>
2018-08-09 19:38:34 +0300

    Update tt_content.php

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,2 +1,28 @@
 <?php
 defined('TYPO3_MODE') or die();
+$fields = [
+    'tt_content_feuser_user' => [
+        'exclude' => true,
+        'label' => 'LLL:EXT:news_feuser/Resources/Private/Language/locallang.xlf:tx_news_domain_model_news.tx_newsfeuser_user',
+        'config' => [
+            'type' => 'group',
+            'internal_type' => 'db',
+            'allowed' => 'fe_users',
+            'foreign_table' => 'fe_users',
+            'size' => 10,
+            'minitems' => 0,
+            'maxitems' => 1010,
+            'default' => '',
+            'wizards' => [
+                'suggest' => [
+                    'type' => 'suggest',
+                    'default' => [
+                        'searchWholePhrase' => true
+                    ]
+                ],
+            ],
+        ]
+    ]
+];
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', $fields);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('tt_content', 'tt_content_feuser_user', '', 'after:fe_group');
